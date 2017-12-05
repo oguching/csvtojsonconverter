9174e132d4007ca63f2e444b3915ab8fe6a993e0
Oguchi Nzekwe <nzekwe@gmail.com>
2017-12-05 21:15:27 +0100

    swapped xo for standard

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ csv to json converter assignment on edx introduction to nodejs course
 
 Issues  
 * At first attempt I named the repo csvtojson and tried to install the `csvtojson` module. Npm complained so I changed my project name.
+* Code linting is a great idea by the way. I was using `xo` but found that `standard` fits my style better.
 
 Meta  
-* I'm using `ava` for testing and `xo` for code linting
+* I'm using `ava` for testing and `standard` for code linting
--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 'use strict'
 
 // the program will accept a csv file from commandline
-// if one does not exist, then it will use the one 
+// if one does not exist, then it will use the one
 // found in test/data
 
 // const csvtojson = require('csvtojson')
-// const fs = require('fs')
+const fs = require('fs')
 const path = require('path')
 
 // const acceptCSV = (file = '')
 
 // console.log(csvtojson())
 
-console.log(__dirname)
+const foldername = 'data'
+
+if (fs.existsSync(path.join(__dirname, foldername))) {
+  console.log('file exists')
+} else {
+  console.log('no file here')
+}
+
+// console.log(path.join(__dirname, foldername))
