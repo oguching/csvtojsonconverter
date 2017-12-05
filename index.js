ec643caad494c4418308430320defbea899ddc31
Oguchi Nzekwe <nzekwe@gmail.com>
2017-12-05 20:39:49 +0100

    create test.js and performed simple housekeeping

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict'
 
-const csvtojson = require('csvtojson')
+// the program will accept a csv file from commandline
+// if one does not exist, then it will use the one 
+// found in test/data
 
-console.log(csvtojson())
+// const csvtojson = require('csvtojson')
+// const fs = require('fs')
+const path = require('path')
+
+// const acceptCSV = (file = '')
+
+// console.log(csvtojson())
+
+console.log(__dirname)
